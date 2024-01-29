1622ccbb2cd5b965a5b7fe678ba0de216fef35e1
wildananugrah <wildananugrah@gmail.com>
2024-01-29 21:36:49 +0700

    update minio use ssl to string

--- configs/minio.config.js
+++ configs/minio.config.js
@@ -6,4 +6,4 @@ export const minioHost = process.env.MINIO_HOST
 export const minioPort = parseInt(process.env.MINIO_PORT)
 export const minioAccessKey = process.env.MINIO_ACCESS_KEY
 export const minioSecretKey = process.env.MINIO_SECRET_KEY
-export const minioUseSsl = process.env.MINIO_USE_SSL.toUpperCase() === 'TRUE' ? true : false
+export const minioUseSsl = String(process.env.MINIO_USE_SSL).toUpperCase() === 'TRUE' ? true : false
